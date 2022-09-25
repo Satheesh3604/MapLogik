0b6411054b3b081af4c6aa984d9ccea843beaae0
satheesh <satheesh3604@gmail.com>
2022-09-24 20:28:21 -0700

    initial commit

---
+++ eclipse-workspace/com.adm.examples/src/com/adm/example/For_Loop_Ex.java
@@ -0,0 +1,24 @@
+package com.adm.example;
+
+public class For_Loop_Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ADM");
+		int a=5;
+		for(int i=5;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		for(int x=2;x<=a;x++) {
+			for(int z=1;z<=x;z++) {
+				System.out.print(x);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
